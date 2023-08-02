546dd73c4032e5626c9f558334a191d500999107
Lee <lhy2128@naver.com>
2023-08-02 21:42:13 +0900

    업로드
    ㄴㅇㄹㄴㄹㄴㅇㄹ

---
+++ TrayTool/ToggleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TrayTool.Command;
+
+namespace TrayTool
+{
+    public class ToggleCommand : Command
+    {
+        private Control mControl;
+
+        public ToggleCommand(Control icon) 
+            : base(0, SpecialKey.ALT + SpecialKey.CTRL, (int)System.Windows.Forms.Keys.Q)
+        {
+            mControl = icon;
+        }
+
+        public override void Execute()
+        {
+            mControl.ContextMenuStrip.Show(mControl, Cursor.Position);
+        }
+    }
+}
